fb3351b7cc3fe10f1c83ff4cf0beea6260792fc3
nasty <anastaishastil2002@gmail.com>
2022-09-04 19:44:53 +0300

    Updated

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.conditions.finalTask;
 public class BitwiseValuesSwap {
     public void swap(int first, int second) {
         first ^= second;
-        second ^= second;
+        second ^= first;
         first ^= second;
     }
 }
